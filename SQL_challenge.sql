2b2642baa90d5c3e56ce4e9b2844010736eb01bb
FabulousErin <77863014+FabulousErin@users.noreply.github.com>
2021-04-11 18:50:11 -0500

    Add files via upload

---
+++ SQL_challenge.sql
@@ -0,0 +1,41 @@
+Create database company_db;
+use company_db;
+Create table dept (
+
+dept_no VARCHAR(255),
+dept_name VARCHAR(255),
+primary key (dept_no));
+
+Create table titles (
+
+title_id VARCHAR(255),
+title VARCHAR(255),
+primary key (title_id));
+
+Create table employees (
+emp_no INT,
+emp_title VARCHAR(255),
+birth_date DATE,
+first_name VARCHAR(255),
+last_name VARCHAR(255),
+sex VARCHAR(255),
+hire_date DATE,
+primary key (emp_no));
+
+Create table salaries (
+emp_no INT,
+salary INT,
+primary key (emp_no));
+
+Create table dept_emp (
+emp_no INT,
+dept_no VARCHAR(255),
+primary key (emp_no));
+
+Create table dept_manager (
+dep_no VARCHAR(255),
+emp_no INT,
+primary key (emp_no));
+
+
+
